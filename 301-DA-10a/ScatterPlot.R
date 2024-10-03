cb888c0a60718f6eeb80062523c367a3d376fb16
VarshiniLaksh <115557650+VarshiniLaksh@users.noreply.github.com>
2024-10-03 19:17:31 +0530

    Add files via upload

---
+++ 301-DA-10a/ScatterPlot.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point(size = 3) + # Adds points
+  labs(title = "Scatter Plot of Sepal Dimensions",
+       x = "Sepal Length (cm)",
+       y = "Sepal Width (cm)") + 
+  theme_minimal()
